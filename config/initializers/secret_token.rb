7369a2cb9761a9af01dfcaf915ab57fd69ce618c
dickeyxxx <jeff@dickey.xxx>
2012-08-17 18:06:49 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Letsgrababeer::Application.config.secret_token = 'c2c3ac47ab4ee0e3122ec477cab700aeefe15f2eb68c3acaa70710212c649572ec1c1e9fe0f102863078f6b51626119a819c3404d80a06f729257d30f2ad0995'
